5a91670acdef191b53140b674560d7c2e2f0d673
Mon Sarder <mssarder@cpp.edu>
2026-02-02 21:54:38 -0800

    update School.java and Class.java

--- Class.java
+++ Class.java
@@ -2,131 +2,127 @@ import java.util.List;
 import java.util.ArrayList;
 
 /**
- * Represents the college system.
- * Manages the complete ecosystem of instructors, students, classes, and lecture halls.
+ * Represents an academic course section.
+ * Manages course information including name, time, location, instructor, prerequisites, and enrolled students.
  */
-public class School {
-    /* List of all classes */
-    private List<Class> classes;
+public class Class {
+    /* Course name */
+    private String name;
 
-    /* List of all instructors */
-    private List<Instructor> instructors;
+    /* Scheduled class time */
+    private String time;
 
-    /* List of all students */
-    private List<Student> students;
+    /* Prerequisites for the class */
+    private List<Class> prerequisites;
 
-    /* List of all lecture halls */
-    private List<LectureHall> halls;
+    /* Lecture hall location*/
+    private LectureHall location;
 
-    /*Constructs a new School with empty lists. */
-    public School() {
-        this.classes = new ArrayList<>();
-        this.instructors = new ArrayList<>();
-        this.students = new ArrayList<>();
-        this.halls = new ArrayList<>();
+    /* Lecture instructor/professor */
+    private Instructor instructor;
+
+    /* Roster of enrolled students */
+    private List<Student> roster;
+
+    /**
+     * Constructs a new Class.
+     * @param name The name of the course
+     * @param time The scheduled time for the class
+     */
+    public Class(String name, String time) {
+        this.name = name;
+        this.time = time;
+        this.prerequisites = new ArrayList<>();
+        this.roster = new ArrayList<>();
     }
 
     /**
-     * Creates a new class.
-     * @param name
-     * @param time
-     * @return The newly created Class
+     * Gets the class name.
+     * @return The name of the class
      */
-    public Class createClass(String name, String time) {
-        return null;
+    public String getName() {
+        return name;
     }
 
     /**
-     * Deletes a class.
-     * @param c The class to be deleted
+     * Gets the class time.
+     * @return The scheduled time
      */
-    public void deleteClass(Class c) {}
-
-    /**
-     * Creates a new lecture hall.
-     * @param location
-     * @return The newly created LectureHall
-     */
-    public LectureHall createLectureHall(String location) {
-        return null;
+    public String getTime() {
+        return time;
     }
 
     /**
-     * Deletes a lecture hall.
-     * @param hall The lecture hall to be deleted
+     * Sets the time for the class.
+     * @param time The new scheduled time
      */
-    public void deleteLectureHall(LectureHall hall) {}
-
-    /**
-     * Hires a new instructor.
-     * @param name The instructor's name
-     * @param id The instructor's ID number
-     * @return The newly hired Instructor
-     */
-    public Instructor hireInstructor(String name, int id) {
-        return null;
+    public void setTime(String time) {
+        this.time = time;
     }
 
     /**
-     * Terminates an instructor's employment.
-     * @param instructor The instructor to be terminated
+     * Assigns a lecture hall to the class.
+     * @param location The lecture hall where the class will be held
      */
-    public void terminateInstructor(Instructor instructor) {}
-
-    /**
-     * Admits a new student.
-     * @param name The student's name
-     * @param id The student's ID number
-     * @return The newly admitted Student
-     */
-    public Student admitStudent(String name, int id) {
-        return null;
+    public void setLocation(LectureHall location) {
+        this.location = location;
     }
 
     /**
-     * Releases a student.
-     * @param student The student to be released
+     * Gets the lecture hall for the class.
+     * @return The assigned lecture hall
      */
-    public void releaseStudent(Student student) {}
+    public LectureHall getLocation() {
+        return location;
+    }
 
     /**
-     * Generates a formatted course catalog.
-     * Lists each class with its prerequisites, times, locations, and instructors.
-     * @return The formatted course catalog string
+     * Assigns an instructor to teach the class.
+     * @param instructor The instructor who will teach the class
      */
-    public String generateCourseCatalog() {
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    /**
+     * Gets the instructor for the class.
+     * @return The assigned instructor
+     */
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    /**
+     * Adds a prerequisite class.
+     * @param prereq The prerequisite class to add
+     */
+    public void addPrerequisite(Class prereq) {}
+
+    /**
+     * Gets the list of prerequisite classes.
+     * @return The list of prerequisites
+     */
+    public List<Class> getPrerequisites() {
+        return prerequisites;
+    }
+
+    /**
+     * Adds a student to the class roster.
+     * @param student The student to be added
+     */
+    public void addStudent(Student student) {}
+
+    /**
+     * Removes a student from the class roster.
+     * @param student The student to be removed
+     */
+    public void removeStudent(Student student) {}
+
+    /**
+     * Generates a formatted roster listing the class details.
+     * @return The formatted roster string
+     */
+    public String generateRoster() {
         return "";
-    }
-
-    /**
-     * Gets the list of all classes.
-     * @return The list of classes
-     */
-    public List<Class> getClasses() {
-        return classes;
-    }
-
-    /**
-     * Gets the list of all instructors.
-     * @return The list of instructors
-     */
-    public List<Instructor> getInstructors() {
-        return instructors;
-    }
-
-    /**
-     * Gets the list of all students.
-     * @return The list of students
-     */
-    public List<Student> getStudents() {
-        return students;
-    }
-
-    /**
-     * Gets the list of all lecture halls.
-     * @return The list of lecture halls
-     */
-    public List<LectureHall> getHalls() {
-        return halls;
     }
 }
--- School.java
+++ School.java
@@ -1,15 +1,132 @@
+import java.util.List;
+import java.util.ArrayList;
+
 /**
- * Represents the system of the College.
- * Manages the cycle of instructors, students, classes, and halls.
+ * Represents the college system.
+ * Manages the complete ecosystem of instructors, students, classes, and lecture halls.
  */
 public class School {
+    /* List of all classes */
     private List<Class> classes;
+
+    /* List of all instructors */
     private List<Instructor> instructors;
+
+    /* List of all students */
     private List<Student> students;
+
+    /* List of all lecture halls */
     private List<LectureHall> halls;
 
-    /* @return A formatted course catalog string. */
+    /*Constructs a new School with empty lists. */
+    public School() {
+        this.classes = new ArrayList<>();
+        this.instructors = new ArrayList<>();
+        this.students = new ArrayList<>();
+        this.halls = new ArrayList<>();
+    }
+
+    /**
+     * Creates a new class.
+     * @param name
+     * @param time
+     * @return The newly created Class
+     */
+    public Class createClass(String name, String time) {
+        return null;
+    }
+
+    /**
+     * Deletes a class.
+     * @param c The class to be deleted
+     */
+    public void deleteClass(Class c) {}
+
+    /**
+     * Creates a new lecture hall.
+     * @param location
+     * @return The newly created LectureHall
+     */
+    public LectureHall createLectureHall(String location) {
+        return null;
+    }
+
+    /**
+     * Deletes a lecture hall.
+     * @param hall The lecture hall to be deleted
+     */
+    public void deleteLectureHall(LectureHall hall) {}
+
+    /**
+     * Hires a new instructor.
+     * @param name The instructor's name
+     * @param id The instructor's ID number
+     * @return The newly hired Instructor
+     */
+    public Instructor hireInstructor(String name, int id) {
+        return null;
+    }
+
+    /**
+     * Terminates an instructor's employment.
+     * @param instructor The instructor to be terminated
+     */
+    public void terminateInstructor(Instructor instructor) {}
+
+    /**
+     * Admits a new student.
+     * @param name The student's name
+     * @param id The student's ID number
+     * @return The newly admitted Student
+     */
+    public Student admitStudent(String name, int id) {
+        return null;
+    }
+
+    /**
+     * Releases a student.
+     * @param student The student to be released
+     */
+    public void releaseStudent(Student student) {}
+
+    /**
+     * Generates a formatted course catalog.
+     * Lists each class with its prerequisites, times, locations, and instructors.
+     * @return The formatted course catalog string
+     */
     public String generateCourseCatalog() {
         return "";
+    }
+
+    /**
+     * Gets the list of all classes.
+     * @return The list of classes
+     */
+    public List<Class> getClasses() {
+        return classes;
+    }
+
+    /**
+     * Gets the list of all instructors.
+     * @return The list of instructors
+     */
+    public List<Instructor> getInstructors() {
+        return instructors;
+    }
+
+    /**
+     * Gets the list of all students.
+     * @return The list of students
+     */
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    /**
+     * Gets the list of all lecture halls.
+     * @return The list of lecture halls
+     */
+    public List<LectureHall> getHalls() {
+        return halls;
     }
 }
